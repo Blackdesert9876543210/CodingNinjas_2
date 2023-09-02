783869aa94f17585cc95aa734c4872f0439fd7cf
AshimBarman <ashlabbarman@gmail.com>
2023-09-02 23:44:49 +0530

    Serving text

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const express = require('express');
 const PORT = 3000;
 const app = express();
 
+app.get('/profile', function (req, res) {
+    res.send('serving text !! ')
+})
+
 app.listen(PORT, function (err) {
     if (err) {
         console.log('Error in running servver',err);
