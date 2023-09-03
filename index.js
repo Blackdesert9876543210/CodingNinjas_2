0bd19ca75fabc377f317458aa7108822ad6dccc3
AshimBarman <ashlabbarman@gmail.com>
2023-09-03 17:22:11 +0530

    read data from contact list and display it in ui

--- index.js
+++ index.js
@@ -7,11 +7,31 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+var contactlist = [
+    {
+        name: "ashim",
+        phone : "888888888888"
+    },
+    {
+        name: "santu",
+        phone: "00000000000"
+    },
+    {
+        name: "kali",
+        phone: "9999999999999"
+    }
+]
+
 app.get('/', function (req, res) {
-    return res.render('home', { title: "contact list"});
+    return res.render('home', { 
+        title: "contact list",
+        contact_list: contactlist
+    });
 });
 app.get('/practice', function(req, res) {
-    return res.render('practice',{ title: "practice ejs page"});
+    return res.render('practice',{ 
+        title: "practice ejs page"
+    });
 })
 
 app.listen(PORT, function (err) {
